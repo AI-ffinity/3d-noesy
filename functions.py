a3cd2442e8773ad11584b4f311bc0e2d7dd52c88
katja <ekaterina.burakova@aiffinity.com>
2024-05-28 23:18:06 +0200

    Cleaned up the notebook by writing more functions, wip

--- functions.py
+++ functions.py
@@ -28,34 +28,49 @@ def tidy_list(df):
     df['res'] = df.res.str.removesuffix('N')
     df.drop(columns='label', inplace=True)
     df['inter'] = df.noe_res != df.res
+
+    df['resnum'] = df['res'].str.extract('(\d+)', expand=False).fillna(0).astype(int)
+    df['noe_resnum'] = df['noe'].str.extract('(\d+)', expand=False).fillna(0).astype(int)
+
     return df
 
-
-def get_n_deviations(df_strong, df_weak):
-    """Calcaulates how many interresidual NOEs are
+def get_n_anomalies(df_strong, df_weak):
+    """Calculates how many interresidual NOEs are
     more intense than the correlation Hn-N-Hnoe
 
     Arguments:
         - df_strong (DataFrame):  list of intra-residual NOE peaks, which are supposed to be the most intense (filtered by the NOE atom type)
         - df_weak (DataFrame): list of inter-residual list of NOE peaks, which are assumed to be less intense (filtered by the NOE atom type, must be the same as in df_i)
     Returns:
-        None
+        n_anomalies (int): number of the
         """
     noes_strong = df_strong[['height', 'res']].groupby('res', as_index=True).max('height')
     noes_weak = df_weak[['height', 'res']].groupby('res', as_index=True).max('height')
 
     noe_compare = noes_strong.join(noes_weak, how='left',
                                    lsuffix='_noe_intra', rsuffix='_noe_inter').fillna(0)
-    n_devs = (noe_compare['height_noe_intra'].apply(np.abs)
+    n_anomalies = (noe_compare['height_noe_intra'].apply(np.abs)
               < noe_compare['height_noe_inter'].apply(np.abs)).sum()
-    print(n_devs)
-    if n_devs == 0:
+    print(n_anomalies)
+    if n_anomalies == 0:
         print("Brilliant result, we have 0 deviations from the theory!")
     else:
         print("There are some deviations, check those most prominent deviations closer:")
         print(noe_compare.loc[
                   noe_compare['height_noe_intra'].apply(np.abs) < noe_compare['height_noe_inter'].apply(np.abs)])
+    return n_anomalies
 
-def get_deviations(df_strong, df_weak):
-    """Get the details about the mismatch"""
+def get_anomalies(df_strong, df_weak):
+    """Get the details about the intensity anomaly"""
+
+    noes_strong = df_strong[['height', 'res']].groupby('res', as_index=True).max('height')
+    noes_weak = df_weak[['height', 'res']].groupby('res', as_index=True).max('height')
+
+    noe_compare = noes_strong.join(noes_weak, how='left',
+                                   lsuffix='_noe_intra', rsuffix='_noe_inter').fillna(0)
+
+    print("There are some deviations, check those most prominent deviations closer:")
+    return (noe_compare.loc[
+              noe_compare['height_noe_intra'].apply(np.abs) < noe_compare['height_noe_inter'].apply(np.abs)])
+
     pass
